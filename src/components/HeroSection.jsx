4e237cb8be1d40c60a5eccbfcefdaed3626e1694
AadityaNayak <aadityanayak7@gmail.com>
2025-01-21 23:29:17 +0530

    Changed hero img path

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -27,7 +27,7 @@ const HeroSection = () => {
   return (
     <section className="relative h-[85%] w-full bg-gradient-to-br from-[#ffffff] via-[#fff6ea] to-[#e6eef8]">
       {/* Background Image */}
-      <div className="hidden lg:block absolute bg-right-bottom h-full bg-[url('static/hero.png')] bg-no-repeat w-full bg-contain z-[0]"></div>
+      <div className="hidden lg:block absolute bg-right-bottom h-full bg-[url('static/hero_img.png')] bg-no-repeat w-full bg-contain z-[0]"></div>
 
       {/* Right and Left Sections */}
       <div className="h-full w-full flex justify-between ">
